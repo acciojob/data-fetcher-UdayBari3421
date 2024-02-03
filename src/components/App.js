488cb93fd8145313cecbdd27995bea538745e962
UdayBari3421 <udaybari2003@gmail.com>
2024-02-03 14:07:50 +0530

    submitting code-Sat Feb 03 2024 14:07:49 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,37 @@
-
-import React from "react";
-import './../styles/App.css';
+import React, { useEffect, useState } from "react";
+import "../styles/App.css";
+import axios from "axios";
 
 const App = () => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  function fetchData() {
+    setLoading(true);
+
+    axios
+      .get("https://dummyjson.com/products")
+      .then((response) => {
+        setData(JSON.stringify(response.data, null, 2));
+        setLoading(false);
+      })
+      .catch((error) => console.log(error));
+  }
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div>
-        {/* Do not remove the main div */}
+      {loading ? (
+        <p>loading...</p>
+      ) : (
+        <div>
+          <h1>Data Fetched from API</h1>
+          <pre>{data}</pre>
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
