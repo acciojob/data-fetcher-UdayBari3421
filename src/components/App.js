439d412dde79334bae37ce9bdbd9129dc903cea9
UdayBari3421 <udaybari2003@gmail.com>
2024-02-03 14:38:20 +0530

    submitting code-Sat Feb 03 2024 14:38:19 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
-import "../styles/App.css";
+import "./../styles/App.css";
 import axios from "axios";
+import { json } from "react-router-dom";
 
 const App = () => {
   const [data, setData] = useState(null);
